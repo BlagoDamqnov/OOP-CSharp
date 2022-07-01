0ad9b25956934e83757f49a9aeb78519a2036755
BlagoDamqnov <chessplayer89@abv.bg>
2022-07-01 10:30:31 +0300

    Inheritance Stack

---
+++ CustomStack/CustomStack/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings:Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count<=0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddRange(IEnumerable<string> elements)
+        {
+            foreach (var item in elements)
+            {
+                Push(item);
+            }
+        }
+    }
+}
---
+++ CustomStack/CustomStack/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings myStack = new StackOfStrings();
+
+            Console.WriteLine(myStack.IsEmpty());
+
+            string[] array = new string[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = $"element{i}";
+            }
+
+            myStack.AddRange(array);
+
+            Console.WriteLine(myStack.Count);
+        }
+    }
+}
