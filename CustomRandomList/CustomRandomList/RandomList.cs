2c84939ecae61c137ab68c73e6a190eb0a81527a
BlagoDamqnov <chessplayer89@abv.bg>
2022-07-01 10:17:47 +0300

    Inheritance a List and remove a random element

---
+++ CustomRandomList/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList:List<string>
+    {
+       public string RandomString(List<string> s)
+        {
+            Random random = new Random();
+
+            var num = random.Next(0,s.Count);
+
+            s.Remove(s[num]);
+            return s[num];
+        }
+    }
+}
---
+++ CustomRandomList/CustomRandomList/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList s = new RandomList();
+
+            s.Add("A");
+            s.Add("B");
+            s.Add("C");
+            s.Add("D");
+            s.Add("E");
+            s.Add("F");
+
+            s.RandomString(s);
+
+            Console.WriteLine(string.Join(" ",s));
+        }
+    }
+}
