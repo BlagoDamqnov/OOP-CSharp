20aecc58146c68b970f3ed5be15b695a05a9e017
BlagoDamqnov <chessplayer89@abv.bg>
2022-07-04 08:14:45 +0300

    commit

---
+++ Person/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public Cake(string name) : base(name, cakePrice, grams, calories)
+        {
+            
+        }
+        private const double grams = 350;
+        private const double calories = 1000;
+        private const decimal cakePrice = 5;
+
+    }
+}
